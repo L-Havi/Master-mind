66d3ec124cb8e65785fb41209d3221114edb6bea
L-Havi <72817588+L-Havi@users.noreply.github.com>
2022-01-23 01:45:49 +0200

    Update and rename sample to InputScanner.java

---
+++ myApp/utils/InputScanner.java
@@ -0,0 +1,27 @@
+package utils;
+
+import java.util.Scanner;
+
+public class InputScanner {
+	private int readInt()
+    {
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextInt();
+    }
+
+    private String readString()
+    {
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextLine();
+    }
+
+    public String[] scanArguments(int argumentsNeeded){
+      
+      String[] arguments = new String[argumentsNeeded];
+
+       for (int i=0; i<argumentsNeeded; i++){
+          arguments[i]=readString();
+       }
+      return arguments;
+    }
+}
